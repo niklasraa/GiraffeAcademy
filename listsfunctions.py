01ca1b717f38a2dd7f2a6c70e232c7c0cf847167
Niklas V. Raa <niklas.vatshelle.raa@gmail.com>
2021-07-17 13:43:27 +0200

    Completed list function, starting tuples

---
+++ listsfunctions.py
@@ -0,0 +1,16 @@
+
+lucky_numbers = [4, 12, 15, 16, 2, 11]
+friends = ["Kevin", "Karen", "Jim", "Toby", "Kenneth", "Jim"]
+
+
+friends.sort()
+lucky_numbers.sort()
+
+friends2 = friends.copy()
+# pop: pops an element of the list
+friends2.pop(3)
+print(friends)
+print(friends2)
+print(lucky_numbers)
+
+
