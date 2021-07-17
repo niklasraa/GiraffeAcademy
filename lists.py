dda5b0419815f0992ade07f25ffcbd7f5d17d4bb
Niklas V. Raa <niklas.vatshelle.raa@gmail.com>
2021-07-17 13:26:18 +0200

    Lists complete

--- lists.py
+++ lists.py
@@ -1,4 +1,10 @@
+# List of friends
+friends = ["Kevin", "Karen", "Jim", "Toby", "Kenneth"]
 
+# start of list = 0
+# end of list = -1
+# 1:3 -> 1 to and not 3 in the list
 
+friends[1] = "Mike"
 
-
+print(friends[1:])
