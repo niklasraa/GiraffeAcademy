44c701c4373a1899a7abb3b130f43e966e48b66f
Niklas V. Raa <niklas.vatshelle.raa@gmail.com>
2021-07-19 14:52:16 +0200

    Functions complete, next up Return Statement

---
+++ functions.py
@@ -0,0 +1,9 @@
+# Function that says hi
+
+# All code after : will be in the function
+def say_hi(name, age):
+    print("Hello " + name + ", You are " + str(age))  # str("") --> converts int to string
+
+
+say_hi("Mike", 32)
+say_hi("Steve", 27)
